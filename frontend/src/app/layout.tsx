42307514a57da876451764b57a9e2c8a0f2b3010
Theekshana Yugan <offmailacc2022@gmail.com>
2025-04-21 12:54:17 +0530

    2nd

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "1st change",
+  title: "2nd       change",
   description: "",
   icons: {
   icon: '/favicon.png',
