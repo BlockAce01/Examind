9c48d41b693e14f72779ade72e8db855d64af485
Gayan <gayanashoka38@gmail.com>
2025-04-21 08:04:19 +0530

    create login page create register page change footer company  name create button,input ui component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-gray-300 p-4 mt-8"> {/* mt-8 adds margin top */}
       <div className="container mx-auto text-center text-sm">
-        © {new Date().getFullYear()} Examind. All rights reserved. | Academic Prototype
+        © {new Date().getFullYear()} Examind. All rights reserved. | gigasync 
       </div>
     </footer>
   );
