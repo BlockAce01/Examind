39f4063c2251d9bffc1a5a43306081aaf9c42e84
Theekshana Yugan <yugankavinda@gmail.com>
2025-07-20 13:13:43 +0530

    refactor: remove custom CORS options for simplified configuration

--- backend/server.js
+++ backend/server.js
@@ -7,23 +7,6 @@ const PORT = process.env.PORT || 3001;
 
 //     middleware
 //enable CORS for all routes
-const allowedOrigins = [
-    'https://examindfrontend.vercel.app',
-    'http://localhost:3000'
-];
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-};
-
-app.use(cors(corsOptions));
 
 //parse JSON request bodies
 app.use(express.json());
